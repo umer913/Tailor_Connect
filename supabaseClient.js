74782edd13dfd2c65c9ba1385d2b7a27710a7221
umer913 <uf9139866@gmail.com>
2026-04-10 03:17:15 +0500

    updated

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://wxufxklshorkbxwkxagx.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4dWZ4a2xzaG9ya2J4d2t4YWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTc5OTcsImV4cCI6MjA3NjA5Mzk5N30.Pw1M4KHbAKB2IAemfJGCN1qi1Ewvy8kFPjISDti-OIU';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
